a4ac2f4870fa214ac04cf22737f780c8a7969e00
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-08 08:37:13 +0300

    Add member fields of Hero class and constructor

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,20 @@
 package models;
+import java.util.*;
 
 public class Hero {
+    private String name;
+    private int age;
+    private String power;
+    private String weakness;
+    private int id;
+    private ArrayList<Hero> instances = new ArrayList<>();
+
+    public Hero(String name, Integer age, String power, String weakness){
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
+        instances.add(this);
+        this.id = instances.size();
+    }
 }
