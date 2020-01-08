7c1c125186f15c3f08e6cbda9077c9e20dd7d2ad
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-08 09:22:56 +0300

    Add member fields and constructor method to the Squad class

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,17 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Squad {
+    private int maxSize;
+    private String squadName;
+    private String cause;
+    private static ArrayList<Squad> allSquads = new ArrayList<>();
+
+    public Squad(Integer maxSize, String squadName, String cause){
+        this.maxSize = maxSize;
+        this.squadName = squadName;
+        this.cause = cause;
+        allSquads.add(this);
+    }
 }
