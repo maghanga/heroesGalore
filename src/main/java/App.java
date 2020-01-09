6a3db2ec8a3b6e69eda4bd4a088940e18e41d098
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-09 12:40:17 +0300

    Add deployment code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.*;
 import static spark.Spark.*;
 
+
 public class App {
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
+
     public static void main(String[] args){
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
         get("/", (req, res) -> {
